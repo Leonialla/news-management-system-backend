47f581e5b639a6cb686e297dda5a62654d109a8a
leon <lswx0925@gmail.com>
2022-12-01 18:01:19 +0800

    chore: init

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    java
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+    }
+}
---
+++ src/test/java/io/github/leonialla/SmokeTest.java
@@ -0,0 +1,13 @@
+package io.github.leonialla;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SmokeTest {
+
+    @Test
+    void one_plus_one_should_equal_two() {
+        assertEquals(2, 1 + 1);
+    }
+}
